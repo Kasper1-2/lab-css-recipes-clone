616a2e98517ecf1ad9dbcbaac22df09ac16ee51b
Diego Cisneros <dcisnerosg28@gmail.com>
2024-10-14 16:50:27 +0200

    Your commit message

---
+++ JavaScript/Functions/Return .js
@@ -0,0 +1,23 @@
+function sum (num1, num2) {
+    return num1 + num2;
+  }
+  
+  // The function invocations below will return the result value
+  sum(4, 5);
+  sum(11.4, 11.4);
+
+  //
+
+  function sum1 (num1, num2) {
+    return num1 + num2;
+  }
+  
+  // Store the value returned from the function in a variable:
+  const result1 = sum1(4, 5);
+  const result2 = sum1(11.4, 11.4);
+  
+  // Once the returned value is saved in a variable
+  // we can start using it in the rest of the program
+  console.log("result1", result1);
+  console.log("result2", result2);
+  
